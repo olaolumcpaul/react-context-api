9e2041f59f7cbc210a369103efbb0b899ce449da
Olaoluwa Oyebode <olaolumcpaul@gmail.com>
2024-07-09 12:04:17 +0100

    Olaoluwa Oyebode_Pull Request

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import { useEffect, useState } from 'react'
-import Header from './components/Header'
-import Tweets from './components/Tweets'
-import RightSide from './components/RightSide'
-import defaultTweets from './assets/data/tweets.js'
-import user from './assets/data/user.js'
+import { useEffect, useState, createContext, useContext } from "react";
+import Header from "./components/Header";
+import Tweets from "./components/Tweets";
+import RightSide from "./components/RightSide";
+import defaultTweets from "./assets/data/tweets.js";
+import user from "./assets/data/user.js";
+
+const MyContext = createContext();
 
 function App() {
-    const [tweets, setTweets] = useState(defaultTweets)
-    const [theme, setTheme] = useState('light');
+  const [tweets, setTweets] = useState(defaultTweets);
+  const [theme, setTheme] = useState("light");
 
-    useEffect(() => {
-        theme === 'light'
-          ? document.body.style.backgroundColor = 'white'
-          : document.body.style.backgroundColor = 'black'
-    }, [theme])
+  useEffect(() => {
+    theme === "light"
+      ? (document.body.style.backgroundColor = "white")
+      : (document.body.style.backgroundColor = "black");
+  }, [theme]);
 
-    return (
-        <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
-        </div>
-    )
+  return (
+    <MyContext.Provider value={{ tweets, setTweets, user, theme, setTheme }}>
+      <div className="container">
+        <Header />
+        <Tweets />
+        <RightSide />
+      </div>
+    </MyContext.Provider>
+  );
 }
 
-export { App };
+export { App, MyContext };
